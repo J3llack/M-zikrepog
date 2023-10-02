38c967d77b142b6faeaee0e851bb51ef985260e6
gktg0011 <143345012+gktg0011@users.noreply.github.com>
2023-10-02 23:06:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "25114508")) #Karışmayın
     API_HASH = os.environ.get("API_HASH", "993ccdfe81548dade420e81bcd6807ce") #Karışmayın
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") #Botunuzun Tokenini Girin .  
-    PLAYLIST_ID = os.environ.get("PLAYLIST_ID", ""
+    PLAYLIST_ID = os.environ.get("PLAYLIST_ID", "")
