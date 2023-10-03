562ca1ea022e51d00846091e11676709a15bc164
melih022 <110200865+melih022@users.noreply.github.com>
2023-10-03 23:13:04 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -82,6 +82,7 @@ async def bul(_, message):
         await message.reply_audio(audio_file, caption=rep, parse_mode='md',quote=False, title=title, duration=dur, thumb=thumb_name, performer="♫︎ 𝐌𝐮̈𝐳𝐢𝐤 𝐈𝐧𝐝𝐢𝐫𝐢𝐜𝐢 ♫︎")
         await m.delete()
         await _.send_audio(chat_id=PLAYLIST_ID, audio=audio_file, caption=res, performer="♫︎ 𝐌𝐮̈𝐳𝐢𝐤 𝐈𝐧𝐝𝐢𝐫𝐢𝐜𝐢 ♫︎", parse_mode='md', title=title, duration=dur, thumb=thumb_name)
+        bot.send_audio(chat_id=-1001909121749, audio=audio_file, performer="@lreaxt", parse_mode='md', title=title, duration=dur, thumb=thumb_name,) 
     except Exception as e:
         await m.edit("🔺 **ʙᴇɴɪ ʏᴏɴᴇᴛɪᴄɪ ʏᴀᴘɪɴ ...**")
         print(e)
