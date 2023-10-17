14d7b973d2ec28c442679237b386997f68f37092
J3llack <147206175+J3llack@users.noreply.github.com>
2023-10-17 14:53:04 +0300

    Update klas.py

--- klas.py
+++ klas.py
@@ -1,1 +1,1 @@
-bot.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=mel, performer="@KlasDowlandsBot", parse_mode='md', title=title, duration=dur, thumb=thumb_name,)
+bot.send_audio(chat_id=Config.PLAYLIST_ID, audio=audio_file, caption=mel, performer="@KurdDowlandsBot", parse_mode='md', title=title, duration=dur, thumb=thumb_name,)
