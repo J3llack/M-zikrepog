9ccb74a12452dc3707a494b0eb47a93005567ab6
J3llack <147206175+J3llack@users.noreply.github.com>
2023-10-20 21:32:29 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -61,8 +61,8 @@ async def bul(_, message):
         thumb = requests.get(thumbnail, allow_redirects=True)
         open(thumb_name, "wb").write(thumb.content)
         duration = results[0]["duration"]
-        kisi = f"{message.from_user.first_name}(tg://user?id=message{message.id})"
-
+        kisi = f"{message.from_user.first_name}(tg://user?id=message{usr.id})"
+        
         mel = f"🏷️ Talep Eden : {kisi}\n\n🕊️ Bot : @KurdDowlandsBot\n\n@KurdMuzikFm 👻"
     except Exception as e:
         await m.edit("➻ **şᴀʀᴋɪ ʙᴜʟᴜɴᴀᴍᴀᴅɪ ...🎶**")
